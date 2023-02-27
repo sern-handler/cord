528855e78beecf5ec00301f3f015fb28111f8604
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-02-26 22:31:24 -0600

    update test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,11 @@
 import { describe, expect, it } from 'vitest'
 import { link } from '../src/tools'
-import * as Consts from '../src/constants'
 describe('link generator', () => {
     
     it("should make version x api base link", () => {
         const version = [7,8,9,10];
         for(const v of version) {
-            const discordAPILink = link(v => `${Consts.BASE_API_URL}/v${v}`);
+            const discordAPILink = link(v => `https://discord.com/api/v${v}`);
             expect(discordAPILink(v)).toBe(`https://discord.com/api/v${v}`)
         }
     })
