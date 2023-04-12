fa5ec58cb65ce17053eb46822a764fb42f4f3bc1
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-04-12 00:25:33 -0500

    remove primitive caching and will add back in future

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import { pipe } from 'fp-ts/function';
+import * as fp from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
-import * as O from 'fp-ts/Option'
 import { Rest } from './rest.js';
-import { createHeart, GatewayOpcodes } from './websocket.js'
+import { createHeart } from './websocket.js'
 import { WebSocket } from 'ws'
-import { BehaviorSubject, filter } from 'rxjs';
+import { filter } from 'rxjs';
 import { makeWSUrl } from './internal/tools.js';
-import * as User from './structures/user.js';
+import { GatewayOpcodes } from './types/dispatch.js';
 
 
 export interface GetGatewayBot {
@@ -39,17 +38,17 @@ export const makeClient = async (options : Options) => {
     token: options.token
   });
 
-  const gatewayBotUrl = new BehaviorSubject<O.Option<string>>(O.none);
-  const gatewayBotPayload = new BehaviorSubject<O.Option<GetGatewayBot>>(O.none);
-  const cacheUrl = (payload:GetGatewayBot):TE.TaskEither<Error, GetGatewayBot> => {
-    gatewayBotUrl.next(O.some(payload.url));
-    return TE.right(payload);
-  }
+  //const gatewayBotUrl = new BehaviorSubject<O.Option<string>>(O.none);
+  //const gatewayBotPayload = new BehaviorSubject<O.Option<GetGatewayBot>>(O.none);
+  // const cacheUrl = (payload:GetGatewayBot):TE.TaskEither<Error, GetGatewayBot> => {
+  // gatewayBotUrl.next(O.some(payload.url));
+  //  return TE.right(payload);
+  // }
 
-  const cacheGatewayPayload = (payload: GetGatewayBot) : TE.TaskEither<Error, GetGatewayBot> => {
-    gatewayBotPayload.next(O.some(payload));
-    return TE.right(payload)
-  }
+  // const cacheGatewayPayload = (payload: GetGatewayBot) : TE.TaskEither<Error, GetGatewayBot> => {
+  //  gatewayBotPayload.next(O.some(payload));
+  //  return TE.right(payload)
+  // }
 
   /*
    * @NeedsRefactor
@@ -57,24 +56,20 @@ export const makeClient = async (options : Options) => {
    * Also caches the Gateway Bot Payload and then returns the full url
    * for a websocket's first 
    * */
-  const url = await pipe(
+  const url = await fp.pipe(
       rest.request("GET /gateway/bot") as TE.TaskEither<Error, GetGatewayBot>,
-      TE.chainFirst(cacheUrl),
-      TE.chainFirst(cacheGatewayPayload),
       TE.map(p => makeWSUrl(p.url, 10, 'json')),
       TE.getOrElse((e) => { throw e }),
     )();
+
   const ws = new WebSocket(url, { perMessageDeflate: false });
   const heart = createHeart(ws, options);
   return {
     on:(name: string) => {
         return heart.bloodStream$
-            .pipe(
-                filter(m => m.op === GatewayOpcodes.Dispatch && m.t === name)
-            )
+            .pipe(filter(m => m.op === GatewayOpcodes.Dispatch && m.t === name))
     },
     login: () => heart.start().subscribe(),
-    clientUser: () => rest.request(User.Endpoints.GetCurrentUser) as TE.TaskEither<Error, User.RawUser>
   };
 };
 
